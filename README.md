c24cdc29547f8f3732b45468b4ab0db89d09f982
Chris Predeek <predeekc@hotmail.com>
2014-06-12 23:56:26 -0700

    Added basic outline for demo steps. Fixed tabs/spaces in Startup.cs.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 aspnet-hello-world
 ==================
+
+TODO:
+  - verify KVM version
+  - kpm restore
+  - k web
+
+Focus on the application pipeline
+
+Each step is tied to a progressive tag in git
+
+Step 1: Basic Handler
+  - Handler function (special case middleware)
+  - Registration using Run
+  - Serves basic HTML content
+
+Step 2: Middleware
+  - Build the middle ware (pre/post processing)
+  - Register the middleware using a helper extension method
+  - Requests now have the cache header in the response
+
+Step 3: Using someone else's middleware
+  - Register the static file handler
+  - See that image now loads
+
+Step 4: Map Middleware
+  - Add map section for /private path
+  - Move custom middleware into this mapped version only
+  - Show that only items in the /private path have the cache response header
+  
--- Startup.cs
+++ Startup.cs
@@ -12,13 +12,13 @@ namespace KWebStartup
     {
         public void Configure(IBuilder app)
         {
-        	// Step 4: Add a pipeline for child folders
-    		app.Map("/private", childApp => {
-				childApp.UseMyPrivateMiddleware();
-				childApp.Run(MyHandler);
-			});
+            // Step 4: Add a pipeline for child folders
+            app.Map("/private", childApp => {
+                childApp.UseMyPrivateMiddleware();
+                childApp.Run(MyHandler);
+            });
 
-    		// Step 3: Use static files middleware
+            // Step 3: Use static files middleware
             app.UseStaticFiles();
 
             // Step 1b: Use athe simple handler
@@ -28,42 +28,42 @@ namespace KWebStartup
         // Step 1a: Build a simple handler
         public Task MyHandler(HttpContext context)
         {
-			Console.WriteLine("{0} - {1}", context.Request.Method, context.Request.Path.ToString());			
+            Console.WriteLine("{0} - {1}", context.Request.Method, context.Request.Path.ToString());            
 
-    		var writer = new StreamWriter(context.Response.Body);
-			writer.Write("<html><head><title>Sample Page</title></head><body>Hello World!<br/><img src='images/helloWorld.png'/></body></html>");
-			writer.Flush();
+            var writer = new StreamWriter(context.Response.Body);
+            writer.Write("<html><head><title>Sample Page</title></head><body>Hello World!<br/><img src='images/helloWorld.png'/></body></html>");
+            writer.Flush();
 
-			return Task.FromResult<object>(null);
+            return Task.FromResult<object>(null);
         }
     }
 
-	// Step 2a: Build a simple header injector middleware (before and after)
+    // Step 2a: Build a simple header injector middleware (before and after)
     public class MyPrivateMiddleware
     {
-    	RequestDelegate _next;
+        RequestDelegate _next;
 
-    	public MyPrivateMiddleware(RequestDelegate next)
-    	{
-    		_next = next;
-    	}
-
-		public async Task Invoke(HttpContext context)
+        public MyPrivateMiddleware(RequestDelegate next)
         {
-    		context.Response.Headers.Set("cache-control", "private, max-age=0, no-cache");
+            _next = next;
+        }
 
-    		await _next(context);
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.Headers.Set("cache-control", "private, max-age=0, no-cache");
 
-    		// Do cleanup work here
+            await _next(context);
+
+            // Do cleanup work here
         }
     }
 
     // Step 2b: Build a simple extension wrapper around middleware registration
     public static class MyPrivateMiddlewareExtensions
     {
-    	public static void UseMyPrivateMiddleware(this IBuilder app)
-    	{
-    		app.Use(next => new MyPrivateMiddleware(next).Invoke);
-    	}
+        public static void UseMyPrivateMiddleware(this IBuilder app)
+        {
+            app.Use(next => new MyPrivateMiddleware(next).Invoke);
+        }
     }
 }
