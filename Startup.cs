3f3fad16f2d96aa06d18040bd010c7f2ea7626ad
Chris Predeek <predeekc@hotmail.com>
2014-06-22 11:13:25 -0700

    Setup Step 1

--- Startup.cs
+++ Startup.cs
@@ -12,15 +12,6 @@ namespace AspNetHelloWorld
     {
         public void Configure(IBuilder app)
         {
-            // Step 4: Add a pipeline for child folders
-            app.Map("/private", childApp => {
-                childApp.UseMyPrivateMiddleware();
-                childApp.Run(MyHandler);
-            });
-
-            // Step 3: Use static files middleware
-            app.UseStaticFiles();
-
             // Step 1b: Use the simple handler
             app.Run(MyHandler);         
         }
@@ -31,35 +22,6 @@ namespace AspNetHelloWorld
             Console.WriteLine("{0} - {1}", context.Request.Method, context.Request.Path.ToString());            
 
             return context.Response.WriteAsync("<html><head><title>Sample Page</title></head><body>Hello World!<br/><img src='images/helloWorld.png'/></body></html>");
-        }
-    }
-
-    // Step 2a: Build a simple header injector middleware (before and after)
-    public class MyPrivateMiddleware
-    {
-        RequestDelegate _next;
-
-        public MyPrivateMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
-
-        public async Task Invoke(HttpContext context)
-        {
-            context.Response.Headers.Set("cache-control", "private, max-age=0, no-cache");
-
-            await _next(context);
-
-            // Do cleanup work here
-        }
-    }
-
-    // Step 2b: Build a simple extension wrapper around middleware registration
-    public static class MyPrivateMiddlewareExtensions
-    {
-        public static void UseMyPrivateMiddleware(this IBuilder app)
-        {
-            app.Use(next => new MyPrivateMiddleware(next).Invoke);
         }
     }
 }
