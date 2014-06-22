28b5663388a6eb4fe2b8435ea715db2b053e4cc9
Chris Predeek <predeekc@hotmail.com>
2014-06-22 11:23:34 -0700

    Setup Step 2

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,9 @@ namespace AspNetHelloWorld
     {
         public void Configure(IBuilder app)
         {
+            // Step 2c: Use the middleware
+            app.UseMyPrivateMiddleware();
+
             // Step 1b: Use the simple handler
             app.Run(MyHandler);         
         }
@@ -23,5 +26,34 @@ namespace AspNetHelloWorld
 
             return context.Response.WriteAsync("<html><head><title>Sample Page</title></head><body>Hello World!<br/><img src='images/helloWorld.png'/></body></html>");
         }
+    }
+
+    // Step 2a: Build a simple header injector middleware (before and after)
+    public class MyPrivateMiddleware
+    {
+      RequestDelegate _next;
+
+      public MyPrivateMiddleware(RequestDelegate next)
+      {
+        _next = next;
+      }
+
+      public async Task Invoke(HttpContext context)
+      {
+        context.Response.Headers.Set("cache-control", "private, max-age=0, no-cache");
+
+        await _next(context);
+
+        // Do cleanup work here
+      }
+    }
+
+    // Step 2b: Build a simple extension wrapper around middleware registration
+    public static class MyPrivateMiddlewareExtensions
+    {
+      public static void UseMyPrivateMiddleware(this IBuilder app)
+      {
+        app.Use(next => new MyPrivateMiddleware(next).Invoke);
+      }
     }
 }
